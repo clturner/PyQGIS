924ef580f9aa6a37513b150faa72e14165acb25e
clturner <clturner23@gmail.com>
2018-05-17 05:23:31 +0000

    counts all points in a point layer that fall within every feature of a polyfgon layer

---
+++ PointsInPolygons.py
@@ -0,0 +1,18 @@
+#!/env/bin/python
+
+"""
+Counts the number of points in a points layer that fall within every feature 
+of a polygon layer.
+
+Creates a new polygon shapefile with an attribute column with resulting count of
+points within each polygon feature
+
+"""
+from qgis.core import *
+import processing
+
+polygon_layer = "<PATH TO POLYGON LAYER>"
+point_layer = "<PATH TO POINTS LAYER>"
+attribute_column_name = 'NUMPOINTS'
+Result = "<PATH TO NEW SHAPEFILE>"
+processing.runalg("qgis:countpointsinpolygon", polygon_layer, point_layer, attribute_column_name, Result)
